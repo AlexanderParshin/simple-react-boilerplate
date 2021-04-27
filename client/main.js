c19cb3c3484b420476674793264330adf046864a
U-OTDEL35\aip <aip@l351lion.OTDEL35.local>
2021-04-27 14:37:45 +0300

    start simple react boilerplate

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './main.scss'
+
+const Home = () => {
+  return <div>Hello World</div>
+
+}
+
+const target = document.getElementById('root')
+
+ReactDOM.render(<Home />, target);
---
+++ webpack.dev.config.js
@@ -0,0 +1,70 @@
+const {resolve} = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
+
+const config = {
+  entry: "./client/main.js",
+  mode: "development",
+  output: {
+    filename: "js/[name].bundle.js",
+    path: resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, "dist"),
+    port: 8080,
+    host: "localhost",
+    index: "index.html",
+    overlay: {
+      warning: false,
+      errors: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: 'css/main.css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${__dirname}/client/index.html`,
+          to: "index.html",
+        },
+      ],
+    }),
+  ],
+};
+
+
+
+
+
+module.exports = config
